26f232398d35ff6877ff652f3a0cd88a6d9ae4a4
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-07-20 01:49:08 -0300

    Change buttons size to best experience on mobile

--- pdfviewer.php
+++ pdfviewer.php
@@ -68,8 +68,8 @@ body {
     color: black;
     border: none;
     border-radius: 50%;
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
     font-size: 24px;
     display: flex;
     justify-content: center;
